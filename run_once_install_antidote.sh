0b09d80cbb12e8f4351677a42d0feeccdae08ff8
rmartinbeni <32068910+rmartinbeni@users.noreply.github.com>
2025-10-17 18:54:22 +0200

    Add Zsh configuration files and Antidote plugin manager setup
    - Created dot_zsh_plugins.txt to manage Zsh plugins with Antidote.
    - Added dot_zshrc for Zsh configuration, including Powerlevel10k prompt and aliases.
    - Introduced dot_zshenv for environment variable settings related to Zsh.
    - Added run_once_install_antidote.sh script for installing Antidote.

---
+++ run_once_install_antidote.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Installing antidote in ${XDG_DATA_HOME:-$HOME/.local/share}/zsh/antidote..."
+#git clone --depth=1 https://github.com/mattmc3/antidote.git \
+#  "${XDG_DATA_HOME:-$HOME/.local/share}/zsh/antidote"
